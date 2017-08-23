81e9f398fa7f887e2bc0cb46ac3075fe5dfc76d5
ZuiSong <378818573@qq.com>
2017-08-23 16:16:23 +0800

    增加测试方法

--- src/test/java/cn/mmooo/AppTest.java
+++ src/test/java/cn/mmooo/AppTest.java
@@ -1,6 +1,7 @@
 package cn.mmooo;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,5 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class AppTest {
-
+    @Test
+    public void Start() {
+        System.out.println("==========Test Starting========");
+    }
 }
